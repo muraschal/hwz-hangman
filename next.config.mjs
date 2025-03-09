8fbe2d6f1225fffcec11c963cc55aa307a40dedd
Marcel <marcel@marcelrapold.com>
2025-03-08 19:43:59 -0500

    Fix: Vercel-Deployment-Konfiguration verbessern

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  output: 'standalone',
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+export default nextConfig; 
